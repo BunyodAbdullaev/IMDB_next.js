4345240dd1e00b79abb2ca0b5c5e35a63047681c
bunyod <bunyodabdullaev1234@gmail.com>
2025-01-24 22:35:35 +0500

    redeploying

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,6 +13,7 @@ export default function Card({ results }) {
 					}`}
 					width={500}
 					height={300}
+					alt={'bunyod'}
 					className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
 				></Image>
 				<div className='p-2'>
